c145383b76682b9449e1b9c00829e330c853f05f
Abhijeet Zingare <abhijitzingare728@gmail.com>
2025-09-07 15:14:41 +0530

    First long code practice

---
+++ 01_practice.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void display(vector<int>&v){
+  for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+  }
+  cout<<endl;
+}
+int main(){
+  vector<int> v;
+  int element,size;
+  {
+    cout<<"Enter the size you want to of vector: ";
+    cin>>size;
+    for(int i=0;i<size;i++){
+  
+    cout<<"Enter the element : ";
+    cin>>element;
+    v.push_back(element);
+
+    }
+  }
+      cout<<"Ascending : ";
+    sort(v.begin(),v.end());
+    display(v);
+    
+    cout<<"descending : ";
+    reverse(v.begin(),v.end());
+    display(v);
+    
+    cout<<"Maximum : "<<*max_element(v.begin(),v.end());
+    cout<<endl;
+  
+    cout<<"Minimum :"<<*min_element(v.begin(),v.end());
+  
+  return 0;
+}
