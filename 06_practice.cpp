8de0956bba9c1823cbf1a818f99934024e6090ec
Abhijeet Zingare <abhijitzingare728@gmail.com>
2025-10-04 19:59:00 +0530

    New codes

---
+++ 06_practice.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<sstream>
+using namespace std;
+int main(){
+  string line;
+  
+  cout<<"enter the sentences: ";
+  getline(cin,line);
+    
+  }
+  map<string,int> freq;
+  string word;
+  
+  stringsstream ss(line)
+  
+  return 0;
+}
---
+++ 07_practice.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int N=10;
+  int sum=0;
+  for(int i=1;i<=N;i++){
+    if(i%3==0){
+      sum+=i;
+    }
+      }
+      cout<<"sum of no."<<sum<<endl;
+  return 0;
+}
